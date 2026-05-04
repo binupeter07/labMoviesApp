96db62749ea262bb6e8f3d1e9bf5d44fe88aeed6
Binu Peter <20100648@setu.ie>
2026-05-05 00:11:43 +0100

    Refactor to allow for new filtering hook, and add the hook.

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,25 @@ import FavouriteMoviesPage from "./pages/FavouriteMoviesPage";
 import MovieReviewPage from "./pages/MovieReviewPage";
 import SiteHeader from './components/SiteHeader';
 import UpcomingMoviesPage from "./pages/UpcomingMoviesPage";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000, 
+      refetchOnWindowFocus: false
+    },
+  },
+});
+
 
 const App = () => {
   return (
-     <BrowserRouter>
+    
+    <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
      <SiteHeader /> 
       <Routes>
         <Route path="/movies/favourites" element={<FavouriteMoviesPage />} />
@@ -20,6 +35,7 @@ const App = () => {
         <Route path="/reviews/:id" element={<MovieReviewPage/>} />
       </Routes>
     </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
